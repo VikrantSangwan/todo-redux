eccf64a79c7bcaec6fe79bf577fd7a01c7b24d62
VikrantSangwan <v.sangwan9876@gmail.com>
2023-02-04 01:16:04 +0530

    Basic UI Todo App

---
+++ src/Components/TodoForm/TodoForm.js
@@ -0,0 +1,10 @@
+function TodoForm() {
+  return (
+    <form>
+      <input type="text" className="form-control" />
+      <button className="btn btn-success float-end mt-2">Add</button>
+    </form>
+  );
+}
+
+export default TodoForm;
---
+++ src/Components/TodoList/TodoList.js
@@ -0,0 +1,13 @@
+import "./TodoList.css";
+
+function TodoList() {
+  return (
+    <ul className="list">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  );
+}
+
+export default TodoList;
